24ffda56a9a68dac85670651cad2d8a30fb81dfc
Nrupesh Mhatre <sanjanamhatre555@gmail.com>
2022-04-03 16:39:35 +0530

    Add multiple person

---
+++ src/main/java/addressbook/UC5/Main.java
@@ -0,0 +1,12 @@
+package addressbook;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to the address book system");
+		AddressBook addressBookMain = new AddressBook();
+		addressBookMain.addMultiplePerson();
+
+
+	}
+	}
